3704e84ba75b6098a16ae20270bc5d58a57a1585
akash <mil10akash@gmail.com>
2018-08-29 12:58:18 +0530

    babeled

--- index.js
+++
@@ -1,7 +0,0 @@
-let eventlist = require('./data/eventlist.json');
-let department = require('./data/dept.json');
-let eventdetail = require('./data/eventdetail');
-
-module.exports = {
-	eventlist,department,eventdetail
-};
---
+++ src/index.js
@@ -0,0 +1,7 @@
+let eventlist = require('../data/eventlist.json');
+let department = require('../data/dept.json');
+let eventdetail = require('../data/eventdetail');
+
+module.exports = {
+	eventlist,department,eventdetail
+};
